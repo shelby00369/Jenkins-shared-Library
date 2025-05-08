4ea4146d9fc3358e7fc2696ede0143b71c5c747a
shelby00369 <shelby00369@gmail.com>
2025-05-08 11:17:20 +0530

    Update build.groovy

--- vars/build.groovy
+++ vars/build.groovy
@@ -2,3 +2,4 @@ def call(string ProjectName, String ImageTag, String DockerHubUser){
   echo "This is for building the code"
     sh "docker build -t ${Jendocktoken}/${ProjectName}:${ImageTag} ."
    echo "code cloning success"
+}
